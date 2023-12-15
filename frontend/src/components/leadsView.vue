f9060a749e53e7455f48a3a8a920f489fb68e41f
rinki <rinkibarkheria@gmail.com>
2023-12-15 10:32:06 +0530

    sidebar updated for super admin

---
+++ frontend/src/components/leadsView.vue
@@ -0,0 +1,24 @@
+<!-- Home.vue -->
+<template>
+    <div>
+     <h2>Leads Component</h2>
+      </div>
+  </template>
+    
+  <script>
+  
+  
+  
+  export default {
+      data() {
+          return {
+            
+          };
+      },
+     
+  };
+  </script>
+    
+  <style>
+  /* Add your styles here */
+  </style>
