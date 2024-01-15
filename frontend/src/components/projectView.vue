30ed4ede043b67c92a8ff942fdf9fb9722d70726
Pushkaraj <puhskaraj.adsgrill@gmail.com>
2024-01-15 16:53:21 +0530

    Edit and Delete Project API

--- frontend/src/components/projectView.vue
+++ frontend/src/components/projectView.vue
@@ -124,6 +124,94 @@
           </div>
         </div>
 
+        <!-- Modal for Edit Project -->
+        <div class="modal fade" ref="createProjectModal" id="editProject" tabindex="-1"
+          aria-labelledby="createProjectLabel" aria-hidden="true" @hidden="editProjects">
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="createProjectLabel">Create Project</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body modalBody">
+                <form @submit="editProjects($event, updateProjectData.id)">
+                  <div class="row">
+                    <div style="color: black;" class="col-md-6 mb-3">
+                      <label for="client_name" class="form-label">Client Name</label>
+                      <v-select v-model="selectedClient" :options="allClients" label="name"
+                        placeholder="Select Client Name" />
+                    </div>
+                    <div class="col-md-6 mb-3">
+                      <label for="reporter_id" class="form-label">Manager</label>
+                      <select class="form-control" v-model="updateProjectData.reporter_id">
+                        <option value="">Select Manager</option>
+                        <option value="18">Abhishek</option>
+                        <!-- <option value="Pawan">Pawan</option> -->
+                        <!-- <option v-for="(tag, index) in tags" :key="index" :value="tag.name">{{
+                          tag.name }}</option> -->
+                      </select>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-6 mb-3">
+                      <label for="name" class="form-label">Project Name</label>
+                      <input type="text" class="form-control" v-model="updateProjectData.name" @input="generateKey"
+                        required>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                      <label for="key" class="form-label">Key</label>
+                      <input type="text" class="form-control" v-model="updateProjectData.key" disabled required>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-6 mb-3">
+                      <label for="type" class="form-label">Type</label>
+                      <select class="form-control" v-model="updateProjectData.type" required>
+                        <option value="">Select Type</option>
+                        <option value="ERP">ERP</option>
+                        <option value="CRM">CRM</option>
+                        <!-- <option v-for="(tag, index) in tags" :key="index" :value="tag.name">{{
+                          tag.name }}</option> -->
+                      </select>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                      <label for="team_lead_id" class="form-label">Team Lead</label>
+                      <select class="form-control" v-model="updateProjectData.team_lead_id" required>
+                        <option value="">Select Team Lead</option>
+                        <option value="7">Shyam</option>
+                        <!-- <option v-for="(tag, index) in tags" :key="index" :value="tag.name">{{
+                          tag.name }}</option> -->
+                      </select>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-6 mb-3">
+                      <label for="tech_stacks" class="form-label">Tech. Stack</label>
+                      <input type="text" class="form-control" v-model="updateProjectData.tech_stacks" required>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                      <label for="host_address" class="form-label">Host Address</label>
+                      <input type="text" class="form-control" v-model="updateProjectData.host_address">
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-12 mb-3">
+                      <label for="projectName" class="form-label">Files</label>
+                      <input type="file" accept=".xlsx, .xlx, .pdf, .doc, .ppt" class="form-control" multiple
+                        @change="handleFileChange">
+                    </div>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"
+                      @click="resetValues">Close</button>
+                    <button type="submit" data-bs-dismiss="modal" class="btn btn-primary">Create</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <vue-progress-bar :progress="uploadProgress" />
 
         <!-- Modal for Create Client -->
@@ -265,17 +353,19 @@
                                         <i v-else class="fas fa-trash text-danger m-3 fa-xs"
                                             style="cursor: not-allowed;"></i> -->
 
-                      <i class="fas fa-pencil-alt text-primary fa-xs pr-4 edit-icon"
+                      <i data-bs-toggle="modal" data-bs-target="#editProject" @click="editModal(project)"
+                        class="fas fa-pencil-alt text-primary fa-xs pr-4 edit-icon"
                         style="margin-left: 20px; cursor: pointer;"></i>
-                      <i class="fas fa-trash text-danger m-3 fa-xs delete-icon" style="cursor: pointer;"></i>
+                      <i @click="deleteProject(project.id)" class="fas fa-trash text-danger m-3 fa-xs delete-icon" style="cursor: pointer;"></i>
                     </td>
                   </tr>
                 </tbody>
               </table>
             </div>
           </div>
-          <PaginationComponent v-if="allProjects.length>itemsPerPage" :currentPage="currentPage" :itemsPerPage="itemsPerPage" :filteredUsers="filteredProjects"
-            :prevPage="prevPage" :nextPage="nextPage" :goToPage="goToPage" />
+          <PaginationComponent v-if="allProjects.length > itemsPerPage" :currentPage="currentPage"
+            :itemsPerPage="itemsPerPage" :filteredUsers="filteredProjects" :prevPage="prevPage" :nextPage="nextPage"
+            :goToPage="goToPage" />
         </div>
 
         <!-- Modal for detailed view -->
@@ -334,6 +424,18 @@ export default {
         team_lead_id: '',
         tech_stacks: '',
         host_address: '',
+      },
+      updateProjectData: {
+        id: '',
+        client_id: '',
+        name: '',
+        reporter_id: '',
+        key: '',
+        type: '',
+        team_lead_id: '',
+        tech_stacks: '',
+        host_address: '',
+        status: '',
       },
       clientData: {
         name: '',
@@ -419,6 +521,27 @@ export default {
         pincode: '',
       }
     },
+    async getProjectManagers(){
+      try {
+        this.$store.commit('showLoader');
+        const response = await axios.get(`${BASE_URL}api/development/getProjectManagers`, {
+          headers: {
+            'Content-Type': "multipart/form-data",
+            token: this.authToken,
+          }
+        })
+        // this.allProjects = response.data.projects;
+        console.log("response",  response.data.projects);
+        this.$store.commit('hideLoader');
+      } catch (error) {
+        new Noty({
+          type: 'error',
+          text: error.message,
+          timeout: 500,
+        }).show()
+        this.$store.commit('hideLoader');
+      }
+    },
     async getProjects() {
       try {
         this.$store.commit('showLoader');
@@ -493,6 +616,98 @@ export default {
         this.$store.commit('hideLoader');
       }
     },
+    async editProjects(e) {
+      e.preventDefault();
+      e.preventDefault();
+
+      if (!this.selectedClient) {
+        alert('Please select a Client');
+        return;
+      }
+
+      this.updateProjectData.client_id = this.selectedClient.id;
+
+      try {
+        this.$store.commit('showLoader');
+        let formData = new FormData();
+        formData.append('id', this.updateProjectData.id);
+        formData.append('client_id', this.updateProjectData.client_id);
+        formData.append('name', this.updateProjectData.name);
+        formData.append('key', this.updateProjectData.key);
+        formData.append('type', this.updateProjectData.type);
+        formData.append('reporter_id', this.updateProjectData.reporter_id);
+        formData.append('host_address', this.updateProjectData.host_address);
+        formData.append('tech_stacks', this.updateProjectData.tech_stacks);
+
+        for (let i = 0; i < this.selectedFiles.length; i++) {
+          formData.append('attachments', this.selectedFiles[i]);
+        }
+
+        const response = await axios.put(`${BASE_URL}api/development/projects`, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            'token': this.authToken,
+          },
+          onUploadProgress: progressEvent => {
+            const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+            this.uploadProgress = percentCompleted;
+          },
+        });
+        if (response.status === 201) {
+          Swal.fire({
+            title: response.data.message,
+            icon: 'success',
+          });
+        }
+        this.getProjects();
+        this.$store.commit('hideLoader');
+        this.resetValues();
+      } catch (error) {
+        new Noty({
+          type: 'error',
+          text: error.response.data.message,
+          timeout: 500,
+        }).show();
+        this.$store.commit('hideLoader');
+      }
+    },
+    editModal(project) {
+      console.log("pro", project);
+      this.updateProjectData = { ...project };
+      this.selectedClient = project.client
+      this.selectedFiles=project.attachments
+      this.isEditModalOpen = true;
+      console.log("uppro", this.updateProjectData);
+      console.log("uppro", this.selectedFiles);
+    },
+    async deleteProject(id){
+      console.log(id);
+      Swal.fire({
+                title: 'Are you sure?',
+                text: 'You won\'t be able to revert this!',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#d33',
+                cancelButtonColor: '#3085d6',
+                confirmButtonText: 'Yes, delete it!'
+            }).then(async (result) => {
+                if (result.isConfirmed) {
+                    try {
+                        const response = await axios.delete(`${BASE_URL}api/development/projects?id=${id}`,{
+                          headers:{
+                            token: this.authToken
+                          }
+                        })
+                        this.getProjects();
+                        Swal.fire('Deleted!', response.data.message, 'success');
+                    } catch (error) {
+                      this.getProjects();
+                      Swal.fire('Deleted!', error.response.data.message, 'success');
+
+                    }
+                }
+            });
+    },
     handleFileChange(e) {
       this.selectedFiles = e.target.files
     },
@@ -566,6 +781,7 @@ export default {
   },
   mounted() {
     this.getClients();
+    this.getProjectManagers();
     this.getProjects();
   }
 };
