f21e927367244e7a189be03ee2f6a150faea43b1
Shantanu <shantanukhandelwal41@gmail.com>
2024-04-23 12:56:03 +0530

    roles creation fixed and salesView fixed

--- backend/adsgrill_cms/micro_services/users_management/roles.py
+++ backend/adsgrill_cms/micro_services/users_management/roles.py
@@ -5,8 +5,26 @@ from app.models import Users, Roles
 from django.http import JsonResponse, HttpResponse
 import json
 from django.db import transaction
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import AuthenticationFailed
+from braces.views import CsrfExemptMixin
 
-class RolesView(APIView):
+
+class CsrfExemptSessionAuthentication(SessionAuthentication):
+    def enforce_csrf(self, request):
+        return
+    
+class CustomSessionAuthentication(SessionAuthentication):
+    def authenticate(self, request):
+        user_auth_tuple = super().authenticate(request)
+        if user_auth_tuple is None:
+            raise AuthenticationFailed('Your session has expired. Please log in again.')
+        return user_auth_tuple
+
+class RolesView(CsrfExemptMixin, APIView):
+    authentication_classes = [CsrfExemptSessionAuthentication, CustomSessionAuthentication]
+    permission_classes = [IsAuthenticated]
     def get(self, request):
         if request.method == 'GET':
             requestedRoles = list(Roles.objects.all().values())
