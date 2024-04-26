10861bef9bf820791fefb3c568583e37693135b2
pushkaraj123 <pushkarajlolusare24@gmail.com>
2024-04-26 18:34:30 +0530

    Todo

--- frontend/src/views/Dashboards/DashboardSales.vue
+++ frontend/src/views/Dashboards/DashboardSales.vue
@@ -1,4 +1,12 @@
 <template>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Webkit | Responsive Bootstrap 4 Admin Dashboard Template</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/noty@3.2.0-beta-deprecated/lib/noty.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/noty@3.2.0-beta-deprecated/lib/themes/mint.css">
+    </head>
     <!-- <h1>Dashboard Lead</h1> -->
     <div class="row">
         <div class="col-lg-12">
@@ -39,14 +47,14 @@
                             <table class="table align-items-center">
                                 <tbody>
                                     <tr v-for="(lead, index) in allLeads" :key="index">
-                                        <td >
+                                        <td>
                                             <div class="text-center">
                                                 <!-- <div>
                                                     <img :src="sale.flag" alt="Country flag" />
                                                 </div> -->
                                                 <!-- <div class="ms-4"> -->
-                                                    <p class="mb-0 text-xs font-weight-bold">Contact name:</p>
-                                                    <h6 class="mb-0 text-sm">{{ lead.name }}</h6>
+                                                <p class="mb-0 text-xs font-weight-bold">Contact name:</p>
+                                                <h6 class="mb-0 text-sm">{{ lead.name }}</h6>
                                                 <!-- </div> -->
                                             </div>
                                         </td>
@@ -70,8 +78,10 @@
                                         </td>
                                         <td class="text-sm align-middle">
                                             <div class="text-center col">
-                                                <p v-if="lead?.follow_date" class="mb-0 text-xs font-weight-bold">Follow up's date:</p>
-                                                <h6 class="mb-0 text-sm">{{ lead?.follow_date? formatDate(lead?.follow_date) :"No follow up's" }}</h6>
+                                                <p v-if="lead?.follow_date" class="mb-0 text-xs font-weight-bold">Follow
+                                                    up's date:</p>
+                                                <h6 class="mb-0 text-sm">{{ lead?.follow_date ?
+                                                    formatDate(lead?.follow_date) : "No follow up's" }}</h6>
                                             </div>
                                         </td>
                                     </tr>
@@ -81,7 +91,113 @@
                     </div>
                 </div>
                 <div class="col-lg-5">
-                    <categories-card />
+                    <div class="card">
+                        <div class="p-3 pb-0 card-header">
+                            <div class="d-flex justify-content-between">
+                                <h6 class="mb-2">My Todos</h6>
+                                <div class="dropdown">
+                                    <button class="btn btn-secondary dropdown-toggle" type="button" id="filterDropdown"
+                                        data-bs-toggle="dropdown" aria-expanded="false">
+                                        Filter
+                                    </button>
+                                    <ul class="dropdown-menu" aria-labelledby="filterDropdown">
+                                        <li><a class="dropdown-item" href="#" @click="filterTodos('All')">All</a></li>
+                                        <li><a class="dropdown-item" href="#" @click="filterTodos('todo')">Todo</a></li>
+                                        <li><a class="dropdown-item" href="#" @click="filterTodos('done')">Done</a></li>
+                                    </ul>
+                                </div>
+                                <button data-bs-toggle="modal" data-bs-target="#createTodo" class="btn btn-primary">Add
+                                    Todo</button>
+                            </div>
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table align-items-center">
+                                <tbody>
+                                    <tr v-for="(todo, index) in filteredTodos" :key="index">
+                                        <td>
+                                            <div class="card mb-1">
+                                                <div class="card-body d-flex justify-content-between">
+                                                    <p class="mb-0">{{ todo.desc }}</p>
+                                                    <div data-bs-toggle="modal" data-bs-target="#editTodo">
+                                                        <i class="far fa-edit mr-2" @click="editData(todo)"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div data-bs-backdrop="static" class="modal fade" ref="createProject" id="createTodo" tabindex="-1"
+                    aria-labelledby="createProjectLabel" aria-hidden="true" @hidden="createSprints">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content" style="padding-bottom: 0;padding-left: 7px; padding-right: 7px;">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="createProjectLabel">Create Todo</h5>
+                                <button @click="resetValues" ref="createTodo" type="button"
+                                    class="btn-close bg-dark text-xs" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body modalBodyCreate" style="padding-bottom: 0; height: 14rem;">
+                                <form @submit="createTodo($event)">
+                                    <div class="row">
+                                        <div class="col-md-12 mb-3">
+                                            <label for="title" class="form-label">Description</label>
+                                            <textarea type="text" class="form-control" v-model="todo.desc"
+                                                required></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer"
+                                        style="z-index: 999; margin-top: 10px; position: sticky; bottom: 0; background-color: white; ">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"
+                                            @click="resetValues">Close</button>
+                                        <button type="submit" class="btn btn-primary">Create</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div data-bs-backdrop="static" class="modal fade" ref="createProject" id="editTodo" tabindex="-1"
+                    aria-labelledby="createProjectLabel" aria-hidden="true" @hidden="createSprints">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content" style="padding-bottom: 0;padding-left: 7px; padding-right: 7px;">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="createProjectLabel">Edit Todo</h5>
+                                <button @click="resetValues" ref="editTodo" type="button"
+                                    class="btn-close bg-dark text-xs" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body modalBodyCreate" style="padding-bottom: 0; height: 20rem;">
+                                <form @submit="editTodos($event)">
+                                    <div class="row">
+                                        <div class="col-md-12 mb-3">
+                                            <label for="title" class="form-label">Description</label>
+                                            <textarea type="text" class="form-control" v-model="editTodo.desc"
+                                                required></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12 mb-3">
+                                            <label for="title" class="form-label">Status</label>
+                                            <select class="form-select" v-model="editTodo.status" required>
+                                                <option value="todo">To do</option>
+                                                <option value="done">Done</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer"
+                                        style="z-index: 999; margin-top: 10px; position: sticky; bottom: 0; background-color: white; ">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"
+                                            @click="resetValues">Close</button>
+                                        <button type="submit" class="btn btn-primary">Update</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -168,6 +284,16 @@ export default {
             },
             currentPage: 1,
             allLeads: [],
+            todo: {
+                desc: ""
+            },
+            editTodo: {
+                id: '',
+                desc: "",
+                status: ''
+            },
+            allTodos: [],
+            selectedStatus: "All",
         }
     },
     components: {
@@ -175,6 +301,13 @@ export default {
     },
     computed: {
         ...mapState(['authUser', 'authToken']),
+        filteredTodos() {
+            if (this.selectedStatus === "All") {
+                return this.allTodos;
+            } else {
+                return this.allTodos.filter(todo => todo.status === this.selectedStatus);
+            }
+        }
     },
     methods: {
         formatDate(inputDate) {
@@ -186,7 +319,7 @@ export default {
         showFollowUpLeads() {
             this.allLeads = this.allLeads.filter(lead => lead.follow_date);
         },
-        showAllLeads(){
+        showAllLeads() {
             this.getLeads()
         },
         async getLeads() {
@@ -213,9 +346,89 @@ export default {
                 }).show()
             }
         },
+        async getTodos() {
+            try {
+                const response = await axios.get(`${BASE_URL}api/todo/`, {
+                    headers: {
+                        token: this.authToken,
+                    }
+                })
+                if (response.status === 200) {
+                    this.allTodos = response?.data?.todo
+                }
+            } catch (error) {
+                new Noty({
+                    type: 'error',
+                    text: error.response.data.message ? error.response.data.message : error.response.data.detail,
+                    timeout: 1000,
+                }).show()
+            }
+        },
+        async createTodo(e) {
+            e.preventDefault();
+
+            try {
+                const response = await axios.post(`${BASE_URL}api/todo/`, this.todo, {
+                    headers: {
+                        token: this.authToken,
+                    }
+                })
+                if (response.status === 201) {
+                    new Noty({
+                        type: 'success',
+                        text: '✔ Added!',
+                        timeout: 1000,
+                        layout: 'topCenter'
+                    }).show()
+                    this.getTodos();
+                    this.$refs.createTodo.click()
+                }
+            } catch (error) {
+                new Noty({
+                    type: 'error',
+                    text: error.response.data.message ? error.response.data.message : error.response.data.detail,
+                    timeout: 1000,
+                }).show()
+            }
+        },
+        editData(todo) {
+            this.editTodo.id = todo.id;
+            this.editTodo.desc = todo.desc;
+            this.editTodo.status = todo.status;
+        },
+        async editTodos(e) {
+            e.preventDefault();
+            try {
+                const response = await axios.patch(`${BASE_URL}api/todo/`, this.editTodo, {
+                    headers: {
+                        token: this.authToken,
+                    }
+                })
+                if (response.status === 200) {
+                    new Noty({
+                        type: 'success',
+                        text: '✔ Updated!',
+                        timeout: 1000,
+                        layout: 'topCenter'
+                    }).show()
+                    this.getTodos();
+                    this.$refs.editTodo.click()
+                }
+            } catch (error) {
+                new Noty({
+                    type: 'error',
+                    text: error.response.data.message ? error.response.data.message : error.response.data.detail,
+                    timeout: 1000,
+                }).show()
+            }
+        },
+        filterTodos(status) {
+            this.selectedStatus = status;
+        },
     },
     mounted() {
-        this.getLeads()
-    }  
+        this.getLeads();
+        this.getTodos();
+    }
 }
 </script>
